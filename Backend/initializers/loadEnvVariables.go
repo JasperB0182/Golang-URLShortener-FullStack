c3268ee75d2af0d9adf29dd93f4bb90323912548
s1155772 <jbatenburg@ziggo.nl>
2026-06-09 18:04:14 +0200

    chore: Updated build-angular to 22

--- Backend/initializers/loadEnvVariables.go
+++ Backend/initializers/loadEnvVariables.go
@@ -2,14 +2,17 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load()
+	wd, _ := os.Getwd()
+	log.Println("Working directory:", wd)
 
+	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env File")
+		log.Fatal("Error loading .env file")
 	}
 }
