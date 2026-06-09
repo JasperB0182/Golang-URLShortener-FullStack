c3147bc2156037b3b4a405482d0b3e097f668300
s1155772 <jbatenburg@ziggo.nl>
2026-06-09 19:35:12 +0200

    Revert the last 3 commits

--- Backend/initializers/loadEnvVariables.go
+++ Backend/initializers/loadEnvVariables.go
@@ -2,17 +2,14 @@ package initializers
 
 import (
 	"log"
-	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
-	wd, _ := os.Getwd()
-	log.Println("Working directory:", wd)
+	err := godotenv.Load()
 
-	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env File")
 	}
 }
